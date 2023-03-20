293abfc5d3c2aebc58b7dcaf1402fec70f57c172
anhntpph28990@fpt.edu.vn <anhntpph28990@fpt.edu.vn>
2023-03-20 19:51:00 +0700

    no message

--- src/phanmembangiay/PhanMemBanGiay.java
+++ src/phanmembangiay/PhanMemBanGiay.java
@@ -15,7 +15,7 @@ public class PhanMemBanGiay {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hi");
+        System.out.println("hello");
     }
     
 }
